86b498794bbe7c5feba0a94a595924c19e5bc8da
Rushaniya <rusha359@gmail.com>
2022-10-18 16:51:08 +0300

    the project has been initialized

---
+++ app.js
@@ -0,0 +1,25 @@
+require('@babel/register');
+
+const express = require('express');
+const { sequelize } = require('./db/models');
+
+const configApp = require('./config/serverConfig');
+
+// подключение роутеров
+
+const app = express();
+
+const PORT = process.env.PORT ?? 3000;
+
+configApp(app);
+
+// запуск роутеров
+
+app.listen(PORT, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log(`Server has been started on port ${PORT}`);
+  } catch (e) {
+    console.log(`DB issues! ${e.message}.`);
+  }
+});
---
+++ config/serverConfig.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const reactSSR = require('../middleware/reactSSR');
+const getUser = require('../middleware/getUser');
+
+const sessionConfig = require('./sessionConfig');
+
+module.exports = function configApp(app) {
+  app.use(session(sessionConfig));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(reactSSR);
+  app.use(getUser);
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(express.static('public'));
+  app.use(express.json());
+};
