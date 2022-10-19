e6b8c60f6b31664402e231ef818d77997aa1ca8e
Roman <roman.shemelin31@gmail.com>
2022-10-19 16:39:59 +0300

    LoadPhoto views

--- views/LoadPhoto.jsx
+++ views/LoadPhoto.jsx
@@ -1,15 +1,16 @@
 const React = require('react');
-const Layout = require('../view/Layout');
+const Layout = require('../views/Layout');
 
-module.exports = function Album() {
+module.exports = function Album({ user }) {
     return (
-      <Layout>
-          <h1> Загрузка изображения</h1>
-          <form id='upload' method='POST' action='upload' encType='multipart/form-data'>
-              <input type="file" name="image" />
-              <input type="submit" />
-          </form>
-          
+        <Layout user={user}>
+            <h1> Загрузка изображения</h1>
+            <form id='upload' method='POST' action='/upload' encType='multipart/form-data'>
+                <input id='preload' type="file" name="image" />
+                <input type="submit" />
+            </form>
+            <img id="viewImg" />
+            <script defer src="/js/preload.js" />
         </Layout>
     )
 }
