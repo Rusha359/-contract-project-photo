6cca723ddaa7678cf578b141485ca96f135479bb
Ulyana <lizsong691@gmail.com>
2022-10-19 16:09:08 +0300

    Edit seeds

--- db/seeders/20221019094214-users.js
+++ db/seeders/20221019094214-users.js
@@ -1,25 +1,30 @@
 const bcrypt = require('bcrypt');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Users', [{
-      name: 'Alex',
-      email: 'example@mail.ru',
-      password: await bcrypt.hash('12345678', 10),
-      avatar: 'https://tipik.ru/wp-content/uploads/2021/02/%D0%A1%D0%BA%D0%B0%D1%87%D0%B0%D1%82%D1%8C-%D1%81%D0%BC%D0%B5%D1%88%D0%BD%D1%8B%D0%B5-%D1%84%D0%BE%D1%82%D0%BA%D0%B8-%D0%BD%D0%B0-%D0%B0%D0%B2%D1%83-%D0%BF%D1%80%D0%B8%D0%BA%D0%BE%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8-1.jpeg',
+  async up(queryInterface) {
+    const userData = [
+      {
+        name: 'Alex',
+        email: 'example@mail.ru',
+        password: await bcrypt.hash('12345678', 10),
+        avatar: 'https://tipik.ru/wp-content/uploads/2021/02/%D0%A1%D0%BA%D0%B0%D1%87%D0%B0%D1%82%D1%8C-%D1%81%D0%BC%D0%B5%D1%88%D0%BD%D1%8B%D0%B5-%D1%84%D0%BE%D1%82%D0%BA%D0%B8-%D0%BD%D0%B0-%D0%B0%D0%B2%D1%83-%D0%BF%D1%80%D0%B8%D0%BA%D0%BE%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8-1.jpeg',
+      },
+      {
+        name: 'Tony',
+        email: 'example2@mail.ru',
+        password: await bcrypt.hash('87654321', 10),
+        avatar: 'https://tipik.ru/wp-content/uploads/2021/02/%D0%A1%D0%BA%D0%B0%D1%87%D0%B0%D1%82%D1%8C-%D1%81%D0%BC%D0%B5%D1%88%D0%BD%D1%8B%D0%B5-%D1%84%D0%BE%D1%82%D0%BA%D0%B8-%D0%BD%D0%B0-%D0%B0%D0%B2%D1%83-%D0%BF%D1%80%D0%B8%D0%BA%D0%BE%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8-14.jpeg',
+      },
+    ];
+    const users = userData.map((user) => ({
+      ...user,
       createdAt: new Date(),
       updatedAt: new Date(),
-    }, {
-      name: 'Tony',
-      email: 'example2@mail.ru',
-      password: await bcrypt.hash('87654321', 10),
-      avatar: 'https://tipik.ru/wp-content/uploads/2021/02/%D0%A1%D0%BA%D0%B0%D1%87%D0%B0%D1%82%D1%8C-%D1%81%D0%BC%D0%B5%D1%88%D0%BD%D1%8B%D0%B5-%D1%84%D0%BE%D1%82%D0%BA%D0%B8-%D0%BD%D0%B0-%D0%B0%D0%B2%D1%83-%D0%BF%D1%80%D0%B8%D0%BA%D0%BE%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8-14.jpeg',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }], {});
+    }));
+    await queryInterface.bulkInsert('Users', users);
   },
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('User', null, {});
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('Users');
   },
 };
--- db/seeders/20221019104242-photos.js
+++ db/seeders/20221019104242-photos.js
@@ -1,45 +1,45 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Photos', [{
-      img: 'https://kulturologia.ru/files/u18476/cote-2.jpg',
-      album_id: 1,
+  async up(queryInterface) {
+    const photoData = [
+      {
+        img: 'https://kulturologia.ru/files/u18476/cote-2.jpg',
+        album_id: 1,
+      },
+      {
+        img: 'https://kulturologia.ru/files/u18476/cote-8.jpg',
+        album_id: 1,
+      },
+      {
+        img: 'https://krasivosti.pro/uploads/posts/2021-03/1616460715_29-p-klassnie-kotiki-foto-koshka-32.jpg',
+        album_id: 1,
+      },
+      {
+        img: 'https://krasivosti.pro/uploads/posts/2021-03/1616460699_53-p-klassnie-kotiki-foto-koshka-59.jpg',
+        album_id: 1,
+      },
+      {
+        img: 'https://krasivosti.pro/uploads/posts/2021-03/1616460675_37-p-klassnie-kotiki-foto-koshka-40.jpg',
+        album_id: 1,
+      },
+      {
+        img: 'https://krasivosti.pro/uploads/posts/2021-03/thumbs/1616460701_34-p-klassnie-kotiki-foto-koshka-37.jpg',
+        album_id: 1,
+      },
+      {
+        img: 'https://krasivosti.pro/uploads/posts/2021-04/1617911551_23-p-smeshnoi-rizhii-kot-25.jpg',
+        album_id: 1,
+      },
+    ];
+    const photos = photoData.map((photo) => ({
+      ...photo,
       createdAt: new Date(),
       updatedAt: new Date(),
-    }, {
-      img: 'https://kulturologia.ru/files/u18476/cote-8.jpg',
-      album_id: 1,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }, {
-      img: 'https://krasivosti.pro/uploads/posts/2021-03/1616460715_29-p-klassnie-kotiki-foto-koshka-32.jpg',
-      album_id: 1,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }, {
-      img: 'https://krasivosti.pro/uploads/posts/2021-03/1616460699_53-p-klassnie-kotiki-foto-koshka-59.jpg',
-      album_id: 1,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }, {
-      img: 'https://krasivosti.pro/uploads/posts/2021-03/1616460675_37-p-klassnie-kotiki-foto-koshka-40.jpg',
-      album_id: 1,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }, {
-      img: 'https://krasivosti.pro/uploads/posts/2021-03/thumbs/1616460701_34-p-klassnie-kotiki-foto-koshka-37.jpg',
-      album_id: 1,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }, {
-      img: 'https://krasivosti.pro/uploads/posts/2021-04/1617911551_23-p-smeshnoi-rizhii-kot-25.jpg',
-      album_id: 1,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }], {});
+    }));
+    await queryInterface.bulkInsert('Photos', photos);
   },
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Photo', null, {});
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('Photos');
   },
 };
