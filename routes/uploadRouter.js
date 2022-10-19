cb8ff95ac0997523b680f957886822d642b94ec4
Roman <roman.shemelin31@gmail.com>
2022-10-19 13:44:19 +0300

    create uploadRouter

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 
 const LoadPhoto = require('../views/LoadPhoto');
+const fileMiddleWare = require('../middleware/multer');
 
 router.get('/', (req, res) => {
   res.renderComponent(LoadPhoto, {});
 });
-router.post('/', (req, res) => {
-
+router.post('/', fileMiddleWare.single('image'), (req, res) => {
+  res.send('фото загружено');
 })
