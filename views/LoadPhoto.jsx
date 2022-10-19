7aa53286b619176fee279826ce9b745e0db662ba
Roman <roman.shemelin31@gmail.com>
2022-10-19 13:35:36 +0300

    add view Loadphoto

---
+++ views/LoadPhoto.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+const Layout = require('../view/Layout');
+
+module.exports = function Album() {
+    return (
+      <Layout>
+          <h1> Загрузка изображения</h1>
+          <form id='upload' method='POST' action='upload' encType='multipart/form-data'>
+              <input type="file" name="image" />
+              <input type="submit" />
+          </form>
+          
+        </Layout>
+    )
+}
