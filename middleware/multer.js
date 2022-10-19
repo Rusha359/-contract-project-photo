63b3591e6560582d9e7f7f7886df615929ed0693
Roman <roman.shemelin31@gmail.com>
2022-10-19 12:40:50 +0300

    create middleware multer

---
+++ middleware/multer.js
@@ -0,0 +1,22 @@
+const multer = require('multer'); // это middleware для фреймворка express для обработки multipart/form-data , нужная в первую очередь при загрузке файлов
+const path = require('path')
+// создаем переменную где будут храниться наши изображения
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'images/');
+  },
+  filename(req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname))
+  }
+});
+// валидация чтобы мы могли загружать только картинки
+const types = ['image/png', 'image/jpeg', 'image/jpg'];
+// функция проверки файла на валидацию
+const fileFilter = (req, file, cb) => {
+  if (types.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+}
+module.exports = multer({ storage, fileFilter })
